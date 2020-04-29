8cf72cead91808fda4f17acf413885628cc1df46
ErinK <erinknowles@protonmail.com>
2020-04-28 22:48:36 -0300

    Town displays coven members

--- src/store/covenStore.js
+++ src/store/covenStore.js
@@ -16,4 +16,29 @@ export async function getData() {
 				store.myCoven = doc.data();
 			});
 		});
+
+	store.myCoven.membersUids.forEach(memberUid => {
+		db.collection("users")
+			.doc(memberUid)
+			.get()
+			.then(doc => {
+				console.log("this member data: ", doc.data());
+				// push no good. get too many
+				//	store.members.push(doc.data());
+
+				// if not exist, push. otherwise, replace.
+				if (
+					!store.members.find(
+						member => member.userUid === doc.data().userUid
+					)
+				) {
+					store.members.push(doc.data());
+				} else {
+					let storeVersionIndex = store.members.findIndex(
+						member => member.userUid === doc.data().userUid
+					);
+					store.members.splice(storeVersionIndex, 1, doc.data());
+				}
+			});
+	});
 }
--- src/views/Town/index.vue
+++ src/views/Town/index.vue
@@ -1,7 +1,18 @@
 <template>
 	<v-container>
 		<h3>Town</h3>
-		<pre>{{ myCoven }}</pre>
+		<v-card>
+			<v-card-title>{{ myCoven.name }}</v-card-title>
+			<v-card-subtitle>{{ myCoven.type }}</v-card-subtitle>
+			<v-card-text>
+				<!-- v-for members -->
+				<div v-for="member in members" :key="member.userUid">
+					<router-link :to="`/user/${member.userUid}`">{{
+						member.username
+					}}</router-link>
+				</div>
+			</v-card-text>
+		</v-card>
 	</v-container>
 </template>
 
@@ -11,12 +22,14 @@ import { store, getData } from "../../store/covenStore";
 export default {
 	data() {
 		return {
-			myCoven: null
+			myCoven: null,
+			members: null
 		};
 	},
 	async created() {
 		await getData();
 		this.myCoven = store.myCoven;
+		this.members = store.members;
 	}
 };
 </script>
